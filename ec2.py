8492b798d62221414862cec92f719721ae209c7b
Shweta Kumari <69496909+shweta-github04@users.noreply.github.com>
2021-01-16 13:05:48 +0530

    Update ec2.py
    update comment

--- ec2.py
+++ ec2.py
@@ -1,3 +1,4 @@
+#this code creates an ec2
 import boto3
 ec2 =  boto3.client('ec2')
 ec2.run_instances(ImageId='ami-0be2609ba883822ec', InstanceType='t2.micro', SubnetId='subnet-07469a71c283170ae', MinCount=1, MaxCount=1)
